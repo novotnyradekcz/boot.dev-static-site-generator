0d14edb502345c4db57ea1bd3459509b8e18eba1
Radek Novotny <novotnyradekcz@gmail.com>
2026-09-18 15:46:19 +0200

    Added TextNode class

---
+++ main.sh
@@ -0,0 +1,1 @@
+python3.13 src/main.py
---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD_TEXT = "bold text"
+    ITALIC_TEXT = "italic text"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
+        self.text: str = text
+        self.text_type: TextType = text_type
+        self.url: str | None = url
+
+    def __eq__(self, other: "TextNode") -> bool:
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
